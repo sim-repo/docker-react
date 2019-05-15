a3cb56d0a54445381c8965594902af1e72a71860
Igor <ivanov.igor.it@gmail.com>
2019-05-15 11:39:50 +0300

    add CI-mode

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+var assert = require('assert');
+
+
+assert.equal("Hello".length, 5);
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  ReactDOM.render(<App />, div);  
 });
